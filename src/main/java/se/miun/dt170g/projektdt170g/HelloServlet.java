0ccdbb48020d2da28f9978a6effdb5ee82380433
jesper <jeni2001@student.miun.se>
2024-01-26 14:16:52 +0100

    tested database, requires database to have a person table with AGE anf LENGTH attributes

--- src/main/java/se/miun/dt170g/projektdt170g/HelloServlet.java
+++ src/main/java/se/miun/dt170g/projektdt170g/HelloServlet.java
@@ -1,28 +1,53 @@
 package se.miun.dt170g.projektdt170g;
 
 import java.io.*;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import javax.sql.DataSource;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    @Resource(name = "jdbc/database")
+    private DataSource dataSource;
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
+        try (Connection conn = dataSource.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT AGE, LENGTH FROM person")) { // Adjusted query
 
-    public void destroy() {
+            StringBuilder responseHtml = new StringBuilder("<html><body>");
+            responseHtml.append("<h1>Database Connection Successful</h1>");
+            responseHtml.append("<table border='1'>");
+            responseHtml.append("<tr><th>Age</th><th>Length (cm)</th></tr>"); // Adjust header names as needed
+
+            while (rs.next()) {
+                int age = rs.getInt("AGE");
+                float length = rs.getFloat("LENGTH"); // Assuming length is stored as a float
+
+                responseHtml.append("<tr>");
+                responseHtml.append("<td>").append(age).append("</td>");
+                responseHtml.append("<td>").append(length).append("</td>");
+                responseHtml.append("</tr>");
+            }
+
+            responseHtml.append("</table>");
+            responseHtml.append("</body></html>");
+
+            response.getWriter().println(responseHtml.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.getWriter().println("<html><body>");
+            response.getWriter().println("<h1>Database Connection Failed: " + e.getMessage() + "</h1>");
+            response.getWriter().println("</body></html>");
+        }
     }
 }
