221ffbc8238f90b781ed5638412907a577beeba1
jesper <jeni2001@student.miun.se>
2024-03-07 10:22:30 +0100

    new database for everything

--- database/dt170gprojekt.sql
+++ database/dt170gprojekt.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2024 at 03:26 PM
--- Server version: 10.4.32-MariaDB
+-- Generation Time: Mar 07, 2024 at 10:20 AM
+-- Server version: 10.4.32-MariaDB-log
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -43,12 +43,18 @@ CREATE TABLE `a_la_carte_menu` (
 --
 
 INSERT INTO `a_la_carte_menu` (`a_la_carte_id`, `name`, `description`, `type`, `price`) VALUES
-(1, 'köttbullar', 'köttbullar och potatis med gräddsås', 'huvudrätt', 99),
-(2, 'carbonara', 'Spaghetti carbonara med pancetta och parmesanost', 'huvudrätt', 119),
-(3, 'lax och potatis', 'grillad lax med potatis och dillsås', 'huvudrätt', 139),
-(4, 'toast skagen', 'en toast med skagenröra ', 'förrätt', 69),
-(5, 'kladdkaka', 'EN TOAST', 'efterrätt', 59),
-(7, 'testmat', 'test', 'huvudrätt', 99);
+(17, 'Oxfile', 'med potatisgratäng och rödvinssås', 'Huvudrätt', 149),
+(18, 'Älg-gryta', 'Älg-gryta med svamp och gräddsås, serveras med råstekt potatis', 'Huvudrätt', 139),
+(19, 'Parmesangratinerad torskrygg', 'serveras med pressad potatis och gröna bönor', 'Huvudrätt', 119),
+(20, 'Pasta carbonara', 'på pecorino och parmesan-ost', 'Huvudrätt', 109),
+(21, 'Toast skagen', 'med löjrom på surdegsbröd', 'Förrätt', 79),
+(22, 'Miso-soppa', 'soppa på miso och sjögräs', 'Förrätt', 59),
+(23, 'Tomatsallad', 'med ruccola och pinjenötter', 'Förrätt', 49),
+(24, 'Vitlöksbröd', 'surdegsbaguette med vitlökssmör', 'Förrätt', 69),
+(25, 'Kladdkaka', 'serveras med grädde', 'Efterrätt', 59),
+(26, 'Pannacotta', 'ChokladPannacotta med hallonsylt', 'Efterrätt', 69),
+(27, 'Pecanpaj', 'paj på Pecannötter, serveras med grädde', 'Efterrätt', 89),
+(28, 'Glass med friterad banan', 'glass som serveras men friterad banan och lönnsirap', 'Efterrätt', 59);
 
 -- --------------------------------------------------------
 
@@ -71,8 +77,7 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`booking_id`, `name`, `telephone`, `amount`, `date`, `time`) VALUES
-(1, 'anders andersson', '761363895', 4, '2024-02-20', '18:30:00'),
-(2, 'jesper', '06512453', 3, '2024-02-21', '19:30:00');
+(11, 'jesper', '0761363895', 4, '2024-03-14', '18:00:00');
 
 -- --------------------------------------------------------
 
@@ -104,7 +109,14 @@ INSERT INTO `drinks` (`drink_id`, `name`, `description`, `price`) VALUES
 (9, 'vatten', 'glas vatten', 0),
 (10, 'vatten', 'glas vatten', 19),
 (11, 'vatten', 'glas vatten', 19),
-(12, 'vatten', 'glas vatten', 19);
+(12, 'vatten', 'glas vatten', 19),
+(13, 'mojito', 'med mynta', 99),
+(14, 'drink', 'test', 0),
+(16, 'Stor stark', 'en stor stark öl, flera sorter', 69),
+(17, 'rött vin', 'ett glas zinfadel', 79),
+(18, 'Cola', 'Coca Cola', 29),
+(19, 'IPA', 'IPA från Örebros bryggeri, 33cl', 79),
+(20, 'Vatten', 'ett glas vatten', 0);
 
 -- --------------------------------------------------------
 
@@ -143,16 +155,17 @@ CREATE TABLE `events` (
   `date` date NOT NULL,
   `time` time NOT NULL,
   `price` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `image_url` varchar(255) NOT NULL
+  `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`event_id`, `name`, `date`, `time`, `price`, `description`, `image_url`) VALUES
-(1, 'vinprov', '2024-02-29', '20:51:52', 299, 'prova vin', '/path/');
+INSERT INTO `events` (`event_id`, `name`, `date`, `time`, `price`, `description`) VALUES
+(12, 'Algortim-stuga', '2024-03-09', '17:00:00', 0, 'Martin Kjellqvist lär er om Bogo-sort'),
+(13, ' tävling Tyngdlyftning för grodor', '2024-03-16', '15:00:00', 100, 'Ta med din Groda och bevisa dess styrka!!!'),
+(14, 'Jazz kväll', '2024-03-22', '21:00:00', 99, 'kom och dansa till obegriplig musik!!');
 
 -- --------------------------------------------------------
 
@@ -174,18 +187,13 @@ CREATE TABLE `lunch_menu` (
 --
 
 INSERT INTO `lunch_menu` (`lunch_id`, `name`, `description`, `date`, `price`) VALUES
-(1, 'köttbullar och potatis', 'köttbullar och potatis med gräddsås och lingonsylt', '2024-02-08', 99),
-(2, 'fiskgratäng', 'fiskgratäng på torsk', '2024-02-09', 99),
-(3, 'prujolökssoppa', 'en soppa med potatis och purjolök', '2024-02-04', 99),
-(5, 'hasselbackspotatis', 'smörgratinerad hasselbackspotatis med lövbiff', '2024-02-12', 99),
-(6, 'vintergryta', 'vintergryta gjord på älgstek, serveras med ris', '2024-02-10', 99),
-(7, 'tagatelli', 'af', '2024-02-20', 99),
-(8, 'hej', 'dfsdf', '2024-02-21', 13),
-(9, 'giu', 'fjk', '2024-02-24', 99),
-(10, 'potatis', 'med kött', '2024-02-26', 99),
-(11, 'glass', 'glass', '2024-02-22', 75),
-(12, 'kaka', 'kaka', '2024-02-28', 15),
-(15, 'mat', 'mat', '2024-02-29', 110);
+(2, 'fiskgratäng', 'fiskgratäng på torsk', '2024-03-06', 99),
+(3, 'purjolökssoppa', 'en soppa med potatis och purjolök och grädde', '2024-03-07', 99),
+(5, 'lövbiff och hasselbackspotatis', 'smörgratinerad hasselbackspotatis med lövbiff och rödvinssås', '2024-03-05', 99),
+(6, 'vintergryta', 'vintergryta gjord på älgstek, serveras med ris', '2024-03-08', 99),
+(21, 'köttbullar och potatis', 'köttbullar och potatis med gräddsås och lingonsylt', '2024-03-04', 99),
+(22, 'Råraka', 'med stekt fläsk och lingonsylt', '2024-03-11', 99),
+(23, 'schnitzel', 'med pommes och sallad', '2024-03-12', 99);
 
 -- --------------------------------------------------------
 
@@ -200,30 +208,6 @@ CREATE TABLE `purchased_a_la_carte` (
   `a_la_carte_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `purchased_a_la_carte`
---
-
-INSERT INTO `purchased_a_la_carte` (`purchased_ID`, `order_id`, `a_la_carte_id`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(33, 53, 1),
-(38, 56, 1),
-(39, 56, 2),
-(40, 57, 1),
-(41, 57, 2),
-(42, 58, 1),
-(43, 58, 2),
-(54, 59, 1),
-(55, 59, 2),
-(56, 59, 5),
-(57, 59, 5),
-(58, 70, 1),
-(59, 70, 2),
-(60, 70, 5),
-(61, 70, 5),
-(62, 70, 5);
-
 -- --------------------------------------------------------
 
 --
@@ -236,21 +220,6 @@ CREATE TABLE `purchased_drinks` (
   `order_id` int(11) NOT NULL,
   `drink_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `purchased_drinks`
---
-
-INSERT INTO `purchased_drinks` (`purchased_ID`, `order_id`, `drink_id`) VALUES
-(1, 1, 1),
-(2, 58, 1),
-(8, 59, 1),
-(9, 59, 12),
-(10, 59, 12),
-(11, 70, 1),
-(12, 70, 12),
-(13, 70, 12),
-(14, 70, 12);
 
 -- --------------------------------------------------------
 
@@ -281,36 +250,6 @@ CREATE TABLE `restaurant_order` (
   `order_status` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `restaurant_order`
---
-
-INSERT INTO `restaurant_order` (`restaurant_order_id`, `status_appetizer`, `status_main`, `status_dessert`, `restaurant_table_id`, `comment`, `order_status`) VALUES
-(1, 'begun', '', '', 1, 'no cheese', NULL),
-(13, 'none', 'begun', 'none', 3, 'test', NULL),
-(14, 'none', 'begun', 'none', 3, 'test', NULL),
-(15, 'none', 'begun', 'none', 3, 'test', NULL),
-(16, 'none', 'begun', 'none', 3, 'test', NULL),
-(17, 'none', 'begun', 'none', 3, 'test', NULL),
-(22, 'none', 'begun', 'none', 3, 'test', NULL),
-(24, 'none', 'begun', 'none', 3, 'test', NULL),
-(33, 'none', 'begun', 'none', 3, 'test', NULL),
-(37, 'none', 'begun', 'none', 3, 'test', NULL),
-(43, 'none', 'begun', 'none', 3, 'test', NULL),
-(44, 'none', 'begun', 'none', 3, 'test', NULL),
-(52, 'none', 'begun', 'none', 3, 'test', NULL),
-(53, 'none', 'begun', 'none', 3, 'test', NULL),
-(56, 'none', 'begun', 'none', 3, 'test', NULL),
-(57, 'none', 'begun', 'none', 3, 'test', NULL),
-(58, 'none', 'begun', 'none', 3, 'test', NULL),
-(59, 'test', 'test', 'test', 3, 'test', NULL),
-(62, 'none', 'begun', 'none', 3, 'test', NULL),
-(64, 'none', 'begun', 'none', 3, 'test', NULL),
-(65, 'none', 'begun', 'none', 3, 'test', NULL),
-(66, 'none', 'begun', 'none', 3, 'test', NULL),
-(69, 'none', 'none', 'maybe', 3, 'hungry people', 1),
-(70, 'test', 'test', 'test', 3, 'test', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -328,15 +267,31 @@ CREATE TABLE `shift` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `table_session`
+-- Table structure for table `tables`
 --
 
-DROP TABLE IF EXISTS `table_session`;
-CREATE TABLE `table_session` (
-  `session_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `tables`;
+CREATE TABLE `tables` (
   `table_number` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL
+  `status` varchar(255) NOT NULL,
+  `table_size` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`table_number`, `status`, `table_size`) VALUES
+(1, 'Free', 4),
+(2, 'Free', 6),
+(3, 'Free', 4),
+(4, 'Free', 4),
+(5, 'Free', 6),
+(6, 'Free', 6),
+(7, 'Free', 2),
+(8, 'Free', 4),
+(9, 'Free', 4),
+(10, 'Free', 8);
 
 --
 -- Indexes for dumped tables
@@ -413,10 +368,10 @@ ALTER TABLE `shift`
   ADD PRIMARY KEY (`shift_id`);
 
 --
--- Indexes for table `table_session`
+-- Indexes for table `tables`
 --
-ALTER TABLE `table_session`
-  ADD PRIMARY KEY (`session_id`);
+ALTER TABLE `tables`
+  ADD PRIMARY KEY (`table_number`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -426,55 +381,55 @@ ALTER TABLE `table_session`
 -- AUTO_INCREMENT for table `a_la_carte_menu`
 --
 ALTER TABLE `a_la_carte_menu`
-  MODIFY `a_la_carte_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `a_la_carte_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `drinks`
 --
 ALTER TABLE `drinks`
-  MODIFY `drink_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `drink_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `lunch_menu`
 --
 ALTER TABLE `lunch_menu`
-  MODIFY `lunch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `lunch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `purchased_a_la_carte`
 --
 ALTER TABLE `purchased_a_la_carte`
-  MODIFY `purchased_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `purchased_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
 
 --
 -- AUTO_INCREMENT for table `purchased_drinks`
 --
 ALTER TABLE `purchased_drinks`
-  MODIFY `purchased_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `purchased_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT for table `restaurant_order`
 --
 ALTER TABLE `restaurant_order`
-  MODIFY `restaurant_order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `restaurant_order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 
 --
--- AUTO_INCREMENT for table `table_session`
+-- AUTO_INCREMENT for table `tables`
 --
-ALTER TABLE `table_session`
-  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tables`
+  MODIFY `table_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
@@ -493,6 +448,15 @@ ALTER TABLE `purchased_a_la_carte`
 ALTER TABLE `purchased_drinks`
   ADD CONSTRAINT `DrinkPurchasedByDrinkID` FOREIGN KEY (`drink_id`) REFERENCES `drinks` (`drink_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `DrinkPurchasedByOrderID` FOREIGN KEY (`order_id`) REFERENCES `restaurant_order` (`restaurant_order_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+DELIMITER $$
+--
+-- Events
+--
+DROP EVENT IF EXISTS `reset_bookings_daily`$$
+CREATE DEFINER=`root`@`localhost` EVENT `reset_bookings_daily` ON SCHEDULE EVERY 1 DAY STARTS '2024-03-06 00:00:00' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE table_session  SET status = "Free"$$
+
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
